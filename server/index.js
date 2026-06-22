85ad79eae21421390aac00ffd5cc887db93a02dc
vadba <musictop65@gmail.com>
2026-06-22 19:53:24 +0300

    edit server

--- server/index.js
+++ server/index.js
@@ -1,27 +1,28 @@
-import express from "express";
-import cors from "cors";
-import { resolve } from "path";
+import express from 'express';
+import cors from 'cors';
+import { resolve } from 'path';
 
-import { userDefaultPath, userRouter } from "./src/users/usersRouter.js";
-import {
-  categoriesDefaultPath,
-  categoryRouter,
-} from "./src/rosdils/CategoriesRouter.js";
-import { postDefaultPath, postRouter } from "./src/posts/PostsRouter.js";
-import { mailDefaultPath, mailRouter } from "./src/mail/mailRouter.js";
+import { userDefaultPath, userRouter } from './src/users/usersRouter.js';
+import { categoriesDefaultPath, categoryRouter } from './src/rosdils/CategoriesRouter.js';
+import { postDefaultPath, postRouter } from './src/posts/PostsRouter.js';
+import { mailDefaultPath, mailRouter } from './src/mail/mailRouter.js';
 
 export const app = express();
 
 app.use(cors({ origin: true, credentials: true }));
 app.use(express.json());
-app.use(express.static(resolve(process.cwd(), "static")));
+app.use(express.static(resolve(process.cwd(), 'static')));
 
 app.use(userDefaultPath, userRouter);
 app.use(mailDefaultPath, mailRouter);
 app.use(categoriesDefaultPath, categoryRouter);
 app.use(postDefaultPath, postRouter);
 
-app.get("*", (req, res) => {
-  res.sendFile(resolve(process.cwd(), "static", "index.html"));
+app.get('*', (req, res) => {
+  res.sendFile(resolve(process.cwd(), 'static', 'index.html'));
   // завдяки цьому при перезавантажені сторінки ми повертаємось на ту самі сторінку
+});
+
+app.get('/tet', (req, res) => {
+  res.json({ status: 'ok', time: new Date().toISOString() });
 });
